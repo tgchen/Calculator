5754366b35550afb7687d1a42156887e9c1df32c
TC Chen <tgchen@wynetech.com>
2012-06-29 21:41:05 -0700

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Eric Chen on 12/6/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Eric Chen on 12/6/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
